6dc3f54cb53ae1ace58a7de76a2dad2397ac49fb
Shivom <59804849+ShivomA@users.noreply.github.com>
2020-10-08 00:40:23 +0530

    Update Cfg.py

--- Cfg.py
+++ Cfg.py
@@ -1,6 +1,8 @@
 Config = {
     "URI": "mongodb://localhost:27017/FileShare",
 
+    "PyMongoTimeOut": 10000,
+
     "ALLOWED_EXTENSIONS": {'jpg', 'gif', 'png', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'pdf', 'csv'},
 
     "UploadLocation": "G:\\Saves\\PyCharm\\File_Share\\uploads",
